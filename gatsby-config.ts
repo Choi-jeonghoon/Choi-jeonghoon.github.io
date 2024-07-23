f7e88b7d30d63140ecc441d29551c0f455e69916
JH <jh@JHui-MacBookPro.local>
2024-07-24 01:36:18 +0900

    init

---
+++ gatsby-config.ts
@@ -0,0 +1,47 @@
+import type { GatsbyConfig } from 'gatsby'
+
+/*
+ ERROR #11331  API.NODE.VALIDATION
+
+Invalid plugin options for "gatsby-source-contentful":
+
+- "accessToken" is required
+- "spaceId" is required
+
+위와같이 에러가 발생하면 아래 코드를 활용하여 환경변수를 로드 시켜준다.
+*/
+require('dotenv').config() //환경변수 로드 시켜주기
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Dev_Apple`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+
+  graphqlTypegen: true,
+  jsxRuntime: 'automatic',
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
+
+export default config
