036b6c4ead535630ebc706b85e522c45fc321d48
JH <jh@JHui-MacBookPro.local>
2024-07-28 20:13:40 +0900

    Add : 레이아웃 적용

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
-export default function Layout() {
+import styled, { createGlobalStyle } from 'styled-components'
+import { TLayoutProps } from '../../types/commonType'
+
+export default function Layout({ children }: TLayoutProps) {
   return (
     <>
-      <h1>Layout Component</h1>
+      <Wrapper>
+        <GlobalStyle></GlobalStyle>
+        <Contents>{children}</Contents>
+      </Wrapper>
     </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard';
+  }
+
+  html,
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper {
+    min-height: 100%;
+    height: 100%;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  min-height: 100%;
+  margin: 0 auto;
+`
+const Contents = styled.div`
+  margin: 80px 0;
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { PageProps, graphql } from 'gatsby'
+import Layout from '../components/common/Layout'
 
 export default function Index({
   data: {
@@ -7,15 +8,23 @@ export default function Index({
   },
 }: PageProps<Queries.IndexPageQuery>) {
   return (
-    <div>
+    <Layout>
       {nodes.map(({ title, slug, date }) => (
         <div key={slug}>
           {title} / {date} / {slug}
         </div>
       ))}
-    </div>
+    </Layout>
   )
 }
+export const Head = () => (
+  <link
+    rel="stylesheet"
+    as="style"
+    crossOrigin="anonymous"
+    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+  />
+)
 
 export const query = graphql`
   query IndexPage {
