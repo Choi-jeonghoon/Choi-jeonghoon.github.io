60fd33df772719d655d8ad7ca13b092e19968278
JH <jh@JHui-MacBookPro.local>
2024-07-29 17:56:22 +0900

    feat : 검색 엔진 최적화

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -51,6 +51,12 @@ const config: GatsbyConfig = {
         stripQueryString: true,
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
   ],
 }
 
